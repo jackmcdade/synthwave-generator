c96855703a31d9f0279f23881efb5aa4d828904c
Jack McDade <jack@jackmcdade.com>
2019-08-09 23:41:14 -0400

    ✨ in the beginning…

---
+++ resources/views/generate.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Synthwave Artist Name Generator</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100">
+    <div class="flex items-center justify-center min-h-screen w-full">
+        <h1 class="text-6xl">{{ $name }}</h1>
+    </div>
+</body>
+</html>
