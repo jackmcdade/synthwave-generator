59b401de82c1358337cfa5ac1eaf4fcb0bc97c69
Jack McDade <jack@jackmcdade.com>
2019-08-10 01:18:55 -0400

    wip

--- resources/views/generate.blade.php
+++ resources/views/generate.blade.php
@@ -1,11 +1,12 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Synthwave Band Name Generator</title>
-    <link href="/css/styles.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.typekit.net/buw1cbh.css">
+    <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body class="bg-dark text-white font-display">
     <div class="flex items-center justify-center relative min-h-screen h-full w-full">
