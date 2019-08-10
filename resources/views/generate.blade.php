db321447c6d29ce41a564ded512d1ac28430a7fa
Jack McDade <jack@jackmcdade.com>
2019-08-10 01:21:21 -0400

    responsely smaller text

--- resources/views/generate.blade.php
+++ resources/views/generate.blade.php
@@ -10,10 +10,10 @@
 </head>
 <body class="bg-dark text-white font-display">
     <div class="flex items-center justify-center relative min-h-screen h-full w-full">
-        <div class="text-center text-white pt-8 absolute leading-tight top-0 w-full font-display text-xl">
+        <div class="text-center text-white pt-8 absolute top-0 w-full font-display text-lg md:text-xl">
             Synthwave Band Name Generator
         </div>
-        <h1 class="text-2xl text-white text-shadow font-display">{{ $name }}</h1>
+        <h1 class="text-xl md:text-2xl text-white leading-tight text-shadow font-display">{{ $name }}</h1>
         <a href="https://jackmcdade.com" class="text-sm hover:text-purple p-4 absolute bottom-0 left-0 text-white">Created by Jack McDade</a>
     </div>
 </body>
